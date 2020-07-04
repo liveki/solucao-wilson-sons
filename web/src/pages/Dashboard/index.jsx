8705b1bb63748a5e1dbae3ea55b79dad82015471
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-07-03 23:21:44 -0300

    adicionado modulo de autenticacao

---
+++ web/src/pages/Dashboard/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Dashboard = () => <h1>Estou no Dashboard</h1>;
+
+export default Dashboard;
---
+++ web/src/pages/SignIn/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useCallback } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import { auth } from '../../services/api';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const history = useHistory();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      history.push('dashboard');
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder="E-mail"
+      />
+
+      <input
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        placeholder="Senha"
+      />
+      <button type="submit">Entrar</button>
+      <Link to="signup">Cadastre-se</Link>
+      <br />
+      {errorMessage && <span>{errorMessage}</span>}
+    </form>
+  );
+};
+
+export default SignIn;
